9c6d23102eea628e59706139fe8fddf4629fc0a1
Bevan Clark <Bevan.Clark@openpolytechnic.ac.nz>
2018-09-05 08:33:44 +1200

    Added IOC

---
+++ CoreApi.InversionOfControl/Configure.cs
@@ -0,0 +1,22 @@
+﻿using CoreApi.Application.Contract;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using CoreApi.Application.Service;
+using CoreApi.Entity.Contracts;
+using CoreApi.Entity.Repositories;
+
+namespace CoreApi.InversionOfControl
+{
+    public class Configure
+    {
+
+        public void ConfigureInversionOfControl(IServiceCollection servicesCollection)
+        {
+            //Services
+            servicesCollection.AddTransient<ICustomerService, CustomerService>();
+
+            //Repositories
+            servicesCollection.AddTransient<ICustomerRepository, CustomerRepository>();
+        }
+    }
+}
